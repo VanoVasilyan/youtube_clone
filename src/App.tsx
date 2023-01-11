fbea01348329024751860aac0482c1c39fba7496
Vano <vanovasilyan@gmail.com>
2023-01-11 19:46:29 +0400

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Feed from './components/Feed/Feed';
+import Navbar from './components/Navbar/Navbar';
+import ChannelDetail from './components/ChannelDetail/ChannelDetail';
+import VideoDetail from './components/VideoDetail/VideoDetail';
+import SearchFeed from './components/SearchFeed/SearchFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div></div>
-  );
-}
+const App = () => (
+  <Box sx={{ backgroundColor: '#000' }}>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Feed />} />
+      <Route path='/video/:id' element={<VideoDetail />} />
+      <Route path='/channel/:id' element={<ChannelDetail />} />
+      <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      <Route path='*' element={<Navigate to={'/'} />} />
+    </Routes>
+  </Box>
+)
 
 export default App;
